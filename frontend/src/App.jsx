a2f5663653e6899e04969952a93be24cd5dca455
Pawel Baglaj <184997716+ProRedQ@users.noreply.github.com>
2026-01-16 13:40:45 +0100

    rewrite main page, move login to main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import ProfilePage from './pages/ProfilePage';
 import ChatPage from './pages/ChatPage';
+import MainPage from './pages/MainPage';
 import Navbar from './components/Navbar';
 import { AuthProvider } from './context/AuthContext';
 
@@ -20,7 +21,7 @@ function App() {
           <Route path="/profile/:user_id" element={<ProfilePage />} />
           <Route path="/chat" element={<ChatPage />} />
           <Route path="/chat/:id" element={<ChatPage />} />
-          <Route path="/" element={<h2>Welcome! Please choose an option from the menu.</h2>} />
+          <Route path="/" element={<MainPage />} />
           <Route path="*" element={<h2>Page not found</h2>} />
         </Routes>
       </div>
---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import api from '../services/api';
+import { useAuth } from '../context/AuthContext';
+
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const { login } = useAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMessage('');
+    try {
+      const response = await api.post('/login', { username, password });
+      
+      setMessage('Login successful! Redirecting...');
+      
+      setTimeout(() => {
+        login(response.data.token);
+      }, 1000); 
+
+    } catch (error) {
+      setMessage(error.response?.data?.message || 'An error occurred during login.');
+    }
+  };
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row', flex: 1, justifyContent: 'space-between', alignItems: 'center', height: '80vh', padding: '0 20px' }}>
+      <div>
+        <h2>Welcome to ChatMe</h2>
+        <p>Please log in to continue.</p>
+      </div>
+      <div>
+        <h2>Logowanie</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Username:</label>
+            <input 
+              type="text" 
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)} 
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input 
+              type="password" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+        {message && <p>{message}</p>}
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
